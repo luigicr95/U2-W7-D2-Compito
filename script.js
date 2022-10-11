90dc76211788a0a71061f81451a65405ad128794
luigicr95 <112467906+luigicr95@users.noreply.github.com>
2022-10-11 15:00:23 +0200

    first exercise

---
+++ script.js
@@ -0,0 +1,19 @@
+class Person {
+  constructor(name, age) {
+    this.nome = name;
+    this.età = age;
+  }
+  compareAge(secondperson) {
+    if (this.età > secondperson.età) {
+      console.log(`${this.nome} è più grande di ${secondperson.nome}`);
+    } else {
+      console.log(`${secondperson.nome} è più grande di ${this.nome}`);
+    }
+  }
+}
+
+let p1 = new Person("Luigi", 26);
+let p2 = new Person("Umberto", 25);
+let p3 = new Person("Andrea", 24);
+
+p1.compareAge(p2);
